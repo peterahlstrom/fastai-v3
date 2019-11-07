ce9ca1fb6a858c5d2e469199be968c5e212a7f84
peterahlstrom <ahlstrom.peter@gmail.com>
2019-11-07 19:43:35 +0100

    fixed direct link to model

--- app/server.py
+++ app/server.py
@@ -11,7 +11,7 @@ from starlette.staticfiles import StaticFiles
 
 # export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 # export_file_url = 'https://drive.google.com/open?id=1qUGhD2f8YyVfOxhdyUOZJIktBWAgeomy'
-export_file_url = 'https://drive.google.com/open?id=1-3vVyfmwXYFOYPCyLAaWwAbiaBHo1lee' #pkl-file
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-3vVyfmwXYFOYPCyLAaWwAbiaBHo1lee' #pkl-file
 export_file_name = 'export.pkl'
 # export_file_name = 'boris_vs_harry.pkl'
 
